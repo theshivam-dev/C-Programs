43e68a4af7147ff1b3678665b475020bcc5dd3cc
SnowdenxEdward <snowdenxedward@gmail.com>
2024-09-08 20:45:18 +0530

    Code.c

--- Code.c
+++ Code.c
@@ -7,8 +7,10 @@ int main()
     int a, b, multi;
     printf("Enter value of a: ");
     scanf("%d", &a);
+    
     printf("Enter value of b: ");
     scanf("%d", &b);
+    
     multi = a * b;
     printf("Multiply: %d\n", multi);
 
