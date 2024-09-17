c94280d09c23747a1702b85fd6f23ce5e424865c
SnowdenxEdward <snowdenxedward@gmail.com>
2024-09-17 20:11:03 +0530

    function.c

--- function.c
+++ function.c
@@ -1,22 +1,69 @@
+// This is a program for Find Area of rectangle,circle,triangle and square with user defined function.
+
 #include <stdio.h>
-#define PI 3.14
+#define PI 3.14159
 
-int circle (); // function declaration
+int rectangle();  // Functions Declarations
+int circle ();
+int triangle ();
+int square ();
+
 int main ()
-
 {
-   circle (); // function calling
-   return 0;
+   rectangle();   // Functions Calling
+   circle ();
+   triangle();
+   square ();
+
 }
 
-int circle () // function definition
-
+int rectangle ()   // Rectangle Function Definition
 {
-   float radius, area;
+   float length,width,area;
+   printf("Enter length of a rectangle : ");
+   scanf("%f",&length);
 
-   printf("enter radius of a circle : ");
+   printf("Enter width of a rectangle : ");
+   scanf("%f",&width);
+
+   area = length * width;
+   printf("Area of a rectangle is : %f \n\n",area);
+}
+
+int circle ()  // Circle Function Definition
+{
+   float radius,area;
+   printf("Enter radius of a circle : ");
    scanf("%f",&radius);
 
    area = PI * radius * radius;
-   printf("area of circle is : %f ",area);
+   printf("Area of a circle is : %f \n\n",area);
+
 }
+
+int triangle ()  // Triangle Function Definition
+{
+   float base,height,area;
+   printf("Enter base of triangle :  ");
+   scanf("%f",&base);
+
+   printf("Enter height of triangle : ");
+   scanf("%f",&height);
+
+   area = base * height * 1 / 2 ;
+   printf("Area of triangle is : %f \n\n",area);
+
+}
+
+int square ()  // Square Function Definition
+{
+   float side,area;
+   printf("Enter side of a square : ");
+   scanf("%f",&side);
+
+   area = side * side;
+   printf("Area of a square is : %f \n\n",area);
+
+}
+
+
