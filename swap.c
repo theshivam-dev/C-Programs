2c821c250ce00353630b6b0b92f2d947f048ea12
SnowdenxEdward <snowdenxedward@gmail.com>
2024-09-26 07:48:16 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,11 @@
+// write a program to swap of two type variable
+
+#include <stdio.h>
+int main ()
+{
+   int a = 10, b = 20,c;
+   c = b;  
+   b = a;
+   a = c;
+   printf("A = %d\nB = %d\n ",a,b);
+}
