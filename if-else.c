9a6979b0f409057123b0c04334802eb9a81afc62
SnowdenxEdward <snowdenxedward@gmail.com>
2024-09-28 20:03:28 +0530

    if-else.c

---
+++ if-else.c
@@ -0,0 +1,19 @@
+// Odd or Even program with if-else 
+
+#include <stdio.h>
+int main ()
+{
+    int a;
+    printf("Enter The First value ");
+    scanf ("%d",&a);
+ 
+    if (a % 2)
+    {
+        printf("This is a Odd Number ");
+    }
+     else 
+     {
+        printf("This ia a Even Number ");
+     }
+     return 0;
+}
